377a1095e847c9ce2c8adea9d1c2b6ff222dc00f
Henry <henrstep@amazon.com>
2019-10-22 16:33:14 -0400

    move files

---
+++ CodeWars/ShoppingList/shoppinglist.js
@@ -0,0 +1,33 @@
+(function () {
+    // console.log("hello");
+
+let sandwiches = 4;
+let salads = 6;
+let wraps = 5;
+let frenchFries = 10;
+
+let totalPrice = sandwiches * 8
+console.log(totalPrice)
+
+totalPrice = totalPrice + (salads * 7)
+console.log(totalPrice)
+
+totalPrice = totalPrice + (wraps * 6.50)
+console.log(totalPrice) 
+
+totalPrice = totalPrice + (frenchFries * 1.20)
+console.log(totalPrice)
+
+console.log(totalPrice);
+
+
+
+
+
+
+
+
+
+
+
+})();
---
+++ CodeWars/SimpleComparison/simplecomparison.js
@@ -0,0 +1,29 @@
+(function () {
+    // console.log("hello");
+    
+function add (a, b)
+    if(a==b){
+        return true
+    }else {
+        return false
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}());
